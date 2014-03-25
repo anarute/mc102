72ca695cc201175648355b079e3792773048c5cf
Ana Rute <anarutecvm@gmail.com>
2014-03-24 21:45:16 -0300

    lista1 & lab2

---
+++ labs/lab2/boas_vindas.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (){
+	int h;
+	printf("Digite a hora atual (sem minutos, um numero entre 0 e 24):\n");
+	scanf("%d",&h);
+	if ((h >= 0) && (h < 12)){
+		printf("Bom dia, bem-vindo ao curso de MC102!");
+	} else if ((h >= 12) && (h < 19)) {
+		printf("Boa tarde, bem-vindo ao curso de MC102!");
+	} else if (h >= 19){
+		printf("Boa noite, bem-vindo ao curso de MC102!");
+	}
+	return 0;
+}
