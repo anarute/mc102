e73b20aa61e0689253f3c982374cca6db9c2bc36
Gustavo Padovan <gustavo.padovan@collabora.co.uk>
2014-03-24 22:48:23 -0300

    more fixes

--- labs/lab2/boas_vindas.c
+++ labs/lab2/boas_vindas.c
@@ -5,11 +5,11 @@ int main (){
 	printf("Digite a hora atual (sem minutos, um numero entre 0 e 24):\n");
 	scanf("%d",&h);
 	if ((h >= 0) && (h < 12)){
-		printf("Bom dia, bem-vindo ao curso de MC102!");
+		printf("Bom dia, bem-vindo ao curso de MC102!\n");
 	} else if ((h >= 12) && (h < 19)) {
-		printf("Boa tarde, bem-vindo ao curso de MC102!");
+		printf("Boa tarde, bem-vindo ao curso de MC102!\n");
 	} else if (h >= 19){
-		printf("Boa noite, bem-vindo ao curso de MC102!");
+		printf("Boa noite, bem-vindo ao curso de MC102!\n");
 	}
 	return 0;
 }
